3424d3390c8d9ac1870f6e42617acb188b458cba
KVerhun <k.verhun@gmail.com>
2016-09-24 11:45:21 +0300

    read file

--- tasks/kverhun/02-Words/main.cpp
+++ tasks/kverhun/02-Words/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <set>
+#include <string>
 
-int main()
+namespace
 {
-	std::cout << "Hello world" << std::endl;
-	system("pause");
-	return 0;
+  std::set<std::string> _ReadDictionaryFromFile(const std::string& file_name)
+  {
+    std::set<std::string> dictionary;
+
+    std::ifstream file(file_name);
+    std::string line;
+    while (std::getline(file, line))
+    {
+      dictionary.insert(line);
+    }
+
+    return dictionary;
+  }
+
+}
+
+
+int main(int i_argc, char** i_argv)
+{
+  if (i_argc < 2)
+    return 1;
+
+  std::string dictionary_file_name(i_argv[1]);
+  auto dictionary = _ReadDictionaryFromFile(dictionary_file_name);
+
+  for (const auto& str : dictionary)
+    std::cout << str << std::endl;
+
+  return 0;
 }
