aec1aaca0422c7bb90219740acd6097089c9ec24
Andrey Kuznetsov <andrey198701@gmail.com>
2016-09-27 13:30:43 +0300

    minor refactoring

--- tasks/a.kuznetsov/src/main/scala/task1/TravellingSalesman.scala
+++ tasks/a.kuznetsov/src/main/scala/task1/TravellingSalesman.scala
@@ -6,11 +6,12 @@ import scala.util.Random
 import scalafx.application.{JFXApp, Platform}
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.collections.ObservableBuffer
-import scalafx.geometry.Insets
 import scalafx.scene.layout.Pane
-import scalafx.scene.shape.{Circle, Line, Rectangle}
+import scalafx.scene.shape.{Circle, Line}
 import scalafx.scene.text.Text
 import scalafx.scene.{Node, Scene}
+
+import scalafx.scene.paint.Color._
 
 
 object TravellingSalesman {
@@ -54,8 +55,6 @@ object TravellingSalesman {
   }
 
 }
-
-import scalafx.scene.paint.Color._
 
 object main {
   val states = Seq((3222, 8618), (3326, 1125), (3444, 9217), (3834, 12129), (3944, 10459), (4145, 7240), (3990, 7531), (3026, 8416), (3344, 8423), (4337, 11611), (3947, 8939), (3946, 8690), (4135, 9336), (3920, 9540), (3811, 8452), (3027, 9111), (4418, 6946), (3858, 7629), (4221, 7130), (4244, 8433), (4457, 9360), (3218, 9010), (3834, 9210), (4635, 1121), (4048, 9641), (3990, 11945), (4312, 7132), (4013, 7446), (3540, 10556), (4239, 7345), (3546, 7838), (4649, 10046), (3957, 8259), (3529, 9730), (4456, 1231), (4015, 7652), (4149, 7124), (3400, 811), (4422, 10020), (3609, 8647), (3016, 9744), (4046, 11153), (4415, 7234), (3732, 7726), (4702, 12254), (3820, 8136), (4304, 8923), (4108, 10449))
