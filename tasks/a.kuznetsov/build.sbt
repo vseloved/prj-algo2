c8d927540441a38c951a927ddbcf2cddc9e9eda0
Andrey Kuznetsov <andrey198701@gmail.com>
2016-09-27 13:28:06 +0300

    salesman visualization

--- tasks/a.kuznetsov/build.sbt
+++ tasks/a.kuznetsov/build.sbt
@@ -4,5 +4,9 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
+libraryDependencies += "org.scalafx" %% "scalafx" % "8.0.92-R10"
+
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.0"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+
+
--- tasks/a.kuznetsov/src/test/scala/task1/TravellingSalesmanTest.scala
+++ tasks/a.kuznetsov/src/test/scala/task1/TravellingSalesmanTest.scala
@@ -7,7 +7,7 @@ import scala.util.Random
 
 class TravellingSalesmanTest extends FunSuite with Matchers {
 
-  val printPoints = (x: Any, y: Any) => println(s"$x:$y")
+  val printPoints = (x: (Int, Int)) => println(s"${x._1}:${x._2}")
 
   val random = new Random()
 
@@ -27,12 +27,12 @@ class TravellingSalesmanTest extends FunSuite with Matchers {
 
   test(" it should complete in n log n time in") {
 
-    val testData = Range(1, 500).map((x: Int) => randomPoints(x * 20))
+    val testData = Range(1, 200).map((x: Int) => randomPoints(x * 20))
 
     loadTest(
       testData.map(x => x.size.toString),
       testData,
-      (x: Seq[(Int, Int)]) => TravellingSalesman.find(x, () => 0, (x, y) => ()))
+      (x: Seq[(Int, Int)]) => TravellingSalesman.find(x, () => 0, (x) => ()))
   }
 
   private def randomPoints(n: Int) = Range(0, n).map(_ => (random.nextInt(1000), random.nextInt(1000)))
