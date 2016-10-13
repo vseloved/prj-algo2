28299f297b8ff723d3cdf950ee6cf4cef26d5661
DmytroMitin <dmitin3@gmail.com>
2016-10-13 18:39:15 +0300

    longest common subsequence , edit distance

--- tasks/dmitin/src/lesson4/edit_distance/App.java
+++ tasks/dmitin/src/lesson4/edit_distance/App.java
@@ -1,10 +1,10 @@
-package lesson4.longest_common_subsequence;
+package lesson4.edit_distance;
 
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Created by dmitin on 01.10.16.
+ * Created by dmitin on 13.10.16.
  */
 public class App {
 
@@ -13,21 +13,26 @@ public class App {
     }
 
     private void run() {
-        System.out.println(lcs("fyord", "worlfd"));
+        System.out.println(editDistance("fyord", "worlfd"));
     }
 
-    private List<Pair<Character>> lcs(String string1, String string2) {
+    private List<Pair<Character>> editDistance(String string1, String string2) {
         int size1 = string1.length();
         int size2 = string2.length();
         int[][] x = new int[size1 + 1][size2 + 1];
         for (int i = 0; i <= size1; i++) {
             for (int j = 0; j <= size2; j++) {
-                if (i == 0 || j == 0) {
-                    x[i][j] = 0;
-                } else if (string1.charAt(i - 1) == string2.charAt(j - 1)) {
-                    x[i][j] = x[i - 1][j - 1] + 1;
+                if (i == 0) {
+                    x[i][j] = j;
+                } else if (j == 0) {
+                    x[i][j] = i;
                 } else {
-                    x[i][j] = Math.max(x[i - 1][j], x[i][j - 1]);
+                    int min = Math.min(x[i - 1][j], x[i][j - 1]);
+                    if (string1.charAt(i - 1) == string2.charAt(j - 1)) {
+                        x[i][j] = Math.min(min + 1, x[i - 1][j - 1]);
+                    } else {
+                        x[i][j] = Math.min(min + 1, x[i - 1][j - 1] + 1);
+                    }
                 }
             }
         }
@@ -36,21 +41,25 @@ public class App {
 //            System.out.println(Arrays.toString(x[i]));
 //        }
 
-        System.out.println("length of LCS: " + x[size1][size2]);
+        System.out.println("edit distance: " + x[size1][size2]);
 
         List<Pair<Character>> res = new LinkedList<>();
         int i = size1;
         int j = size2;
         while (i >= 1 && j >= 1) {
-            if (string1.charAt(i - 1) == string2.charAt(j - 1)) {
-                res.add(0, new Pair<>(string1.charAt(i - 1), string2.charAt(j - 1)));
-                i--;
-                j--;
-            } else if (x[i - 1][j] > x[i][j - 1]) {
+            int a = x[i - 1][j] + 1;
+            int b = x[i][j - 1] + 1;
+            int c = x[i - 1][j - 1] + (string1.charAt(i - 1) == string2.charAt(j - 1) ? 0 : 1);
+            int min = Math.min(Math.min(a, b), c);
+            if (min == a) {
                 res.add(0, new Pair<>(string1.charAt(i - 1), '_'));
                 i--;
-            } else {
+            } else if (min == b) {
                 res.add(0, new Pair<>('_', string2.charAt(j - 1)));
+                j--;
+            } else {
+                res.add(0, new Pair<>(string1.charAt(i - 1), string2.charAt(j - 1)));
+                i--;
                 j--;
             }
         }
