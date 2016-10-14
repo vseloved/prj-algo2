df9ea616cadbdee2e68c6c22ab5aad193ff6d7f7
kverhun <k.verhun@gmail.com>
2016-10-14 19:03:31 +0300

    fix after merge

--- tasks/kverhun/04-LCS/impl/main.cpp
+++ tasks/kverhun/04-LCS/impl/main.cpp
@@ -18,24 +18,14 @@ namespace
     {
         for (const auto& row : i_matrix)
         {
-<<<<<<< HEAD
             for (const auto& elem : row)
             {
                 std::cout << elem << '\t';
-=======
-            for (const auto& element : row)
-            {
-                std::cout << element << '\t';
->>>>>>> origin/LCS_WordsAlignment_Pullrequest
             }
             std::cout << std::endl;
         }
     }
-
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/LCS_WordsAlignment_Pullrequest
+    
     TMatrix _GetLcsDP(const std::string& i_w1, const std::string& i_w2)
     {
         auto lcs_matrix = _ConstructMatrix(i_w1.size() + 1, i_w2.size() + 1);
@@ -62,7 +52,6 @@ namespace
 
         return lcs_matrix;
     }
-<<<<<<< HEAD
     
     std::pair<std::string, std::string> _AlingWords(const std::string& i_w1, const std::string& i_w2)
     {
@@ -125,7 +114,6 @@ namespace
         return std::make_pair(w1_aligned, w2_aligned);
     }
 
-=======
 
     std::string _GetLCS(const std::string& i_w1, const std::string& i_w2)
     {
@@ -152,19 +140,10 @@ namespace
         std::reverse(lcs.begin(), lcs.end());
         return lcs;
     }
->>>>>>> origin/LCS_WordsAlignment_Pullrequest
 }
 
 namespace Tests
 {
-<<<<<<< HEAD
-    class Test
-    {
-    public:
-        Test(
-            const std::string& i_w1, const std::string& i_w2,
-            const std::string& i_w1_aligned_expected, const std::string& i_w2_alinged_expected)
-=======
     class TestLCS
     {
     public:
@@ -172,32 +151,20 @@ namespace Tests
             : m_w1(i_w1)
             , m_w2(i_w2)
             , m_lcs_expected(i_lcs_expected)
->>>>>>> origin/LCS_WordsAlignment_Pullrequest
         { }
 
         bool Run() const
         {
-<<<<<<< HEAD
-            auto aligned_words = _AlingWords(m_w1, m_w2);
-            bool first_ok = (aligned_words.first == m_w1_expected);
-            bool second_ok = (aligned_words.second == m_w2_expected);
-            return first_ok && second_ok;
-=======
             auto lcs = _GetLCS(m_w1, m_w2);
             bool result = m_lcs_expected == lcs;
             return result;
->>>>>>> origin/LCS_WordsAlignment_Pullrequest
         }
 
     private:
         const std::string m_w1;
         const std::string m_w2;
-<<<<<<< HEAD
-        const std::string m_w1_expected;
-        const std::string m_w2_expected;
-=======
+ 
         const std::string m_lcs_expected;
->>>>>>> origin/LCS_WordsAlignment_Pullrequest
     };
 }
 
@@ -208,30 +175,16 @@ int main(int i_argc, char** i_argv)
         std::string w1, w2;
         std::cin >> w1 >> w2;
 
-<<<<<<< HEAD
-        auto aligned_words = _AlingWords(w1, w2);
-        std::cout << aligned_words.first << std::endl << aligned_words.second << std::endl;
-=======
         auto lcs = _GetLCS(w1, w2);
         std::cout << lcs << std::endl;
->>>>>>> origin/LCS_WordsAlignment_Pullrequest
-
     }
     else if (i_argc == 2 && std::string(i_argv[1]) == "--test")
     {
-<<<<<<< HEAD
-        Tests::Test test1("abc", "ac", "abc", "a_c");
-        Tests::Test test2("fyord", "world", "fyor_d", "_world");
-        Tests::Test test3("abcd", "abcd", "abcd", "abcd");
-
-        for (auto& test : { test1, test2, test3 })
-=======
         Tests::TestLCS test_lcs_1("abc", "ac", "ac");
         Tests::TestLCS test_lcs_2("fyord", "world", "ord");
         Tests::TestLCS test_lcs_3("abcd", "abcd", "abcd");
         Tests::TestLCS test_lcs_4("fyord", "worfld", "ord");
         for (auto& test : { test_lcs_1, test_lcs_2, test_lcs_3, test_lcs_4 })
->>>>>>> origin/LCS_WordsAlignment_Pullrequest
         {
             bool result = test.Run();
             std::cout << "Test: " << (result ? "OK" : "FAILED") << std::endl;
