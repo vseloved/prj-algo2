c606afc05dd16e12e6bfcadf968e71da854f08e3
kverhun <k.verhun@gmail.com>
2016-10-16 00:41:32 +0300

    Expand unit test for LCS

--- tasks/kverhun/04-LCS/unit_tests/lcs_test.cpp
+++ tasks/kverhun/04-LCS/unit_tests/lcs_test.cpp
@@ -3,23 +3,24 @@
 
 #include "../impl/lcs.h"
 
-TEST_CASE("Words alignment", "[Words alignment]")
-{
-    SECTION("dummy test")
-    {
-        REQUIRE(1 == 1);
-        //auto aligned_words = _AlingWords("abc", "ac");
-        //REQUIRE(aligned_words.first == "abc");
-        //REQUIRE(aligned_words.second == "a_c");
-    }
-}
-
 TEST_CASE("LCS")
 {
-    //Tests::TestLCS test_lcs_1("abc", "ac", "ac");
     SECTION("Test 1")
     {
         REQUIRE(LCS::GetLCS("abc", "ac") == "ac");
+    }
+
+    SECTION("Test 2")
+    {
+        REQUIRE(LCS::GetLCS("fyord", "world") == "ord");
+    }
+    SECTION("Test 3")
+    {
+        REQUIRE(LCS::GetLCS("abcd", "abcd") == "abcd");
+    }
+    SECTION("Test 4")
+    {
+        REQUIRE(LCS::GetLCS("fyord", "worfld") == "ord");
     }
 
 }
